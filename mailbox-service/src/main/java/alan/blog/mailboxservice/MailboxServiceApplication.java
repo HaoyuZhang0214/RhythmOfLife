056a39fb85a6ecd3a3780beb87a4618ece4cc340
Haoyu Zhang <haoyu99@bu.edu>
2023-07-13 03:13:46 -0400

    Add mailbox service

---
+++ mailbox-service/src/main/java/alan/blog/mailboxservice/MailboxServiceApplication.java
@@ -0,0 +1,13 @@
+package alan.blog.mailboxservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailboxServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailboxServiceApplication.class, args);
+    }
+
+}
---
+++ mailbox-service/src/test/java/alan/blog/mailboxservice/MailboxServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package alan.blog.mailboxservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailboxServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
