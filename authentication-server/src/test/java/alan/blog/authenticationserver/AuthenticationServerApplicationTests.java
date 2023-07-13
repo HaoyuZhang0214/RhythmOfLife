0548e12d1f5c2e00d44be8e34d905e7bb59bec4c
Haoyu Zhang <haoyu99@bu.edu>
2023-07-12 23:27:27 -0400

    Initial commit

---
+++ authentication-server/src/test/java/alan/blog/authenticationserver/AuthenticationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package alan.blog.authenticationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ eureka-center/src/main/java/alan/blog/eurekacenter/EurekaCenterApplication.java
@@ -0,0 +1,13 @@
+package alan.blog.eurekacenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaCenterApplication.class, args);
+    }
+
+}
