6b11d3863ecf2780032b8468666caeeaf92ac57c
Haoyu Zhang <haoyu99@bu.edu>
2023-07-24 17:53:02 -0400

    Build eureka center

--- eureka-center/src/main/java/alan/blog/eurekacenter/EurekaCenterApplication.java
+++ eureka-center/src/main/java/alan/blog/eurekacenter/EurekaCenterApplication.java
@@ -2,7 +2,9 @@ package alan.blog.eurekacenter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaCenterApplication {
 
